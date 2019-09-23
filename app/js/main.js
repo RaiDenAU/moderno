df2335ecce8b8f0859f256a45bd56915a14933b2
RaiDenAU <rad240898@gmail.com>
2019-09-23 18:43:43 +0300

    added products, slider, advantages

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,17 @@
-$(function() {});
+$(function() {
+  var mixer = mixitup(".products__inner-container");
+
+  $(".products__slider-inner").slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true,
+    arrows: false
+  });
+
+  $(".rateYo").rateYo({
+    starWidth: "12px",
+    rating: 5,
+    readOnly: true
+  });
+});
