861b7298954a6eda299551392d063ef153380d9c
RaiDenAU <rad240898@gmail.com>
2019-10-12 14:36:32 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -8,7 +8,33 @@ $(function () {
         slidesToShow: 4,
         slidesToScroll: 4,
         dots: true,
-        arrows: false
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1900,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 930,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+
+        ]
     }
 
     );
